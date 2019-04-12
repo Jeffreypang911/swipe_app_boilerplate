43579866d2c0c2bedba73d207fc78156733e15a7
Jeffreypang911 <Jeffreypang911@gmail.com>
2019-04-12 01:41:16 -0700

    adds Class 2 code

---
+++ Class-2-Code/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './styles.js'
+import Home from './screens/Home.js'
+import { 
+  Text, 
+  View 
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Home/>
+      </View>
+    );
+  }
+}
